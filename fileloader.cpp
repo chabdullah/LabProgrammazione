efd8d0f91e9f372bde28467350f135651700fa3c
chabdullah <chabdullah@hotmail.it>
2019-08-22 13:08:29 +0200

    Aggiunto la classe FileLoader

--- fileloader.cpp
+++ fileloader.cpp
@@ -1,6 +1,46 @@
+#include <QTextStream>
+#include <iostream>
 #include "fileloader.h"
 
-FileLoader::FileLoader()
-{
+void FileLoader::loadFile(){
+    int size = 0, progressBar = 0;
+    float totSize;
+    QFile file(":/files/resources/InstallationLog.txt");
+
+    if(!file.open(QFile::ReadOnly | QFile::Text)){
+    }
+    totSize = file.size();
+    QTextStream in(&file);
+    while (!in.atEnd())
+       {
+          QString line = in.readLine();
+          size += line.size();
+          progressBar = ((size/totSize)*100);
+          notify(progressBar,line);
+       }
+
+    notify(100,QString("Completed!"));
+    file.close();
+
+}
+
+void FileLoader::addObserver(Observer *o){
+    observers.push_back(o);
+}
+
+void FileLoader::removeObserver(Observer *o){
+    observers.removeOne(o);
+}
+
+void FileLoader::notify(int progress, QString line){
+    for(Observer* observer: observers)
+        observer->update(progress, line);
+}
+
+FileLoader::FileLoader(QString name){
+    name = "FileLoader";
+}
+
+FileLoader::~FileLoader(){
 
 }
--- fileloader.h
+++ fileloader.h
@@ -2,10 +2,27 @@
 #define FILELOADER_H
 
 
-class FileLoader
+#include <QFile>
+#include <QList>
+#include "subject.h"
+
+class FileLoader: public Subject
 {
 public:
-    FileLoader();
+    FileLoader(QString name);
+    ~FileLoader();
+
+    void addObserver(Observer *o);
+    void removeObserver(Observer *o);
+    void notify(int progress, QString line);
+
+    void loadFile();
+    QString getName(){return name;}
+
+
+private:
+    QList<Observer*> observers;
+    QString name;
 };
 
 #endif // FILELOADER_H
